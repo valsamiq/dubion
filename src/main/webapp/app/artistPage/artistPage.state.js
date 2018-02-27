cb053587754f91c9d2f2b38d9b43a32452cc8c97
kelvinlt <kelvinleethen@gmail.com>
2018-02-27 12:58:06 +0100

    #30 solucion de views de #21 artistPage y #25 bandPage

---
+++ src/main/webapp/app/artistPage/artistPage.state.js
@@ -0,0 +1,36 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('dubionApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider
+            .state('artistPage', {
+                parent: 'entity',
+                url: '/artistPage',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'dubionApp.artistPage.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/artistPage/artistPage.html',
+                        controller: 'artistPageController',
+                        controllerAs: 'vm'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('artistPage');
+                        $translatePartialLoader.addPart('status');
+                        $translatePartialLoader.addPart('global');
+                        return $translate.refresh();
+                    }]
+                }
+            })
+        }
+})();
