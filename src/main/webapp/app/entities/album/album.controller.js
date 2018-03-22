25bfa9490a90b2dd29f3f5356df8b3c16536dafa
pabloskii <pablo.urbano.murillo@gmail.com>
2018-03-22 12:41:29 +0100

    Modificacion photo Album a String
    (cherry picked from commit 066f384)

--- src/main/webapp/app/entities/album/album.controller.js
+++ src/main/webapp/app/entities/album/album.controller.js
@@ -5,24 +5,17 @@
         .module('dubionApp')
         .controller('AlbumController', AlbumController);
 
-    AlbumController.$inject = ['DataUtils', 'Album'];
+    AlbumController.$inject = ['Album'];
 
-    function AlbumController(DataUtils, Album) {
+    function AlbumController(Album) {
 
         var vm = this;
 
         vm.albums = [];
-        vm.openFile = DataUtils.openFile;
-        vm.byteSize = DataUtils.byteSize;
 
         loadAll();
 
         function loadAll() {
-
-            Album.get({id : 1}, function (data){
-                console.log(data);
-            })
-
             Album.query(function(result) {
                 vm.albums = result;
                 vm.searchQuery = null;
